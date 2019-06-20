bc49d994aef47387f4dd132b8c86b080e9e5d3e1
lords91 <claudio.cavallaro91@gmail.com>
2019-06-20 18:36:40 +0200

    ...

--- app/src/main/java/com/unisa/claudiocavallaro/androidtesi/Persistence/Communicator.java
+++ app/src/main/java/com/unisa/claudiocavallaro/androidtesi/Persistence/Communicator.java
@@ -5,7 +5,9 @@ import com.unisa.claudiocavallaro.androidtesi.Model.ApiKeys;
 import com.unisa.claudiocavallaro.androidtesi.Model.Preferenza;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -17,7 +19,9 @@ public class Communicator {
     private static final String URL = "http://progettotesi.ddns.net:33425/";
 
     public void getPref(final ActivityHome activityHome, String id, String pref){
-        Retrofit retrofit = new Retrofit.Builder().baseUrl(URL).addConverterFactory(GsonConverterFactory.create()).build();
+        OkHttpClient client = new OkHttpClient.Builder().connectTimeout(100, TimeUnit.SECONDS).readTimeout(100, TimeUnit.SECONDS).build();
+
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(URL).client(client).addConverterFactory(GsonConverterFactory.create()).build();
 
         Interface api = retrofit.create(Interface.class);
         Call<Preferenza> call = api.getPhonePref(id, pref);
@@ -43,7 +47,10 @@ public class Communicator {
     }
 
     public void getKey(final ActivityHome activityHome){
-        Retrofit retrofit = new Retrofit.Builder().baseUrl(URL).addConverterFactory(GsonConverterFactory.create()).build();
+        OkHttpClient client = new OkHttpClient.Builder().connectTimeout(1000, TimeUnit.SECONDS).readTimeout(1000, TimeUnit.SECONDS).build();
+
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(URL).client(client).addConverterFactory(GsonConverterFactory.create()).build();
+
         Interface api = retrofit.create(Interface.class);
 
         Call<ApiKeys> call = api.getKeys();
